813b6f992d6d7cde2cb3c2eea3be25245b8f935b
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-03-08 11:10:13 +0000

    debugging

--- docker setup/config-docker.bash
+++ docker setup/config-docker.bash
@@ -1,9 +1,9 @@
 # A CONFIG SCRIPT TO INSTALL AND CONFIGURE DOCKER ON A REMOTE MACHINE
 
-sudo apt upgrade && sudo apt update -y
+sudo apt upgrade -y && sudo apt update -y
 
 # Uninstall any old versions of docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc || echo "Could not remove files"
 
 # Install docker
 curl -fsSL htpps://get.docker.com -o get-docker.sh
