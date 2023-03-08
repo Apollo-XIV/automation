94cb2ebe53ab9eea660a1fc8ff549ee7033b72cd
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-03-08 11:10:59 +0000

    typo

--- docker setup/config-docker.bash
+++ docker setup/config-docker.bash
@@ -6,5 +6,5 @@ sudo apt upgrade -y && sudo apt update -y
 sudo apt-get remove docker docker-engine docker.io containerd runc || echo "Could not remove files"
 
 # Install docker
-curl -fsSL htpps://get.docker.com -o get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
