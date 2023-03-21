c8907ffc0b576460ef55efd7cea2bbab8ed45446
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-03-21 15:25:05 +0000

    Update config-docker-ec2.bash

--- docker setup/config-docker-ec2.bash
+++ docker setup/config-docker-ec2.bash
@@ -1,5 +1,6 @@
 # INSTALL DOCKER ON AMAZON-LINUX
 sudo yum update -y
-sudo amazon-linux-extras install docker
+sudo yum install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+sudo systemctl docker enable
